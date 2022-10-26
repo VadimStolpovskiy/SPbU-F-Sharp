6e49fbacdc635ed7ef9debd62e86d1367774fa2a
Vadim <65338154+VadimStolpovskiy@users.noreply.github.com>
2022-10-27 00:58:52 +0300

    Requested changes

--- src/Homework/Task1.fs
+++ src/Homework/Task1.fs
@@ -1,38 +1,51 @@
-﻿namespace Main
+﻿module Homework.Task1
 
-module Task =
-    
-    // Task 1
-    let exp b n =
-            let mutable result = 1
-            for i = 1 to n do
-                  result <- b * result
-            result
-    
-    // Task 2        
-    let quickExp b n = b ** n
-    
-    // Task 3
-    let min x y =
-        if x < y then x
-        else y
-    
-    let max x y =
-        if x > y then x
-        else y
-    
-    let diff (arr: int array) =
-        let mutable Min = arr[0]
-        let mutable Max = arr[0]
-        
-        for i = 0 to arr.Length - 1 do
-            Min <- min Min arr[i]
-            Max <- max Max arr[i]
-        Max - Min
-        
-     // Task 4
-    let odds a b =
-        let filter = 
-            [|a..b|]
-            |> Array.filter (fun elem -> elem % 2 = 1)
-        filter
+// №1
+let exp (value : float) pow : float =
+    let mutable result = 1.0
+
+    if pow < 0 then
+        for i = pow to -1 do
+            result <- 1.0 / (value * result)
+    else
+        for i = 1 to pow do
+            result <- value * result
+
+    result
+
+// №2
+let rec quickExp (value: float) pow : float =
+    if pow = 1 then value
+    elif pow = 0 then 1
+    else
+        let result = quickExp value (pow / 2)
+
+        if pow % 2 = 0 then
+            if pow < 0 then 1.0 / (result * result)
+            else result * result
+        else
+            if pow < 0 then 1.0 / (value * result * result)
+            else value * result * result
+
+
+// №3
+let diff (arr: int array) =
+    if arr.Length = 0 then
+        failwith
+            "The array is empty"
+
+    let mutable tempMin = arr[0]
+    let mutable tempMax = arr[0]
+
+    for i = 0 to arr.Length - 1 do
+        tempMin <- min tempMin arr[i]
+        tempMax <- max tempMax arr[i]
+
+    tempMax - tempMin
+
+// №4
+let odds a b =
+    if a % 2 = 1 then
+        [|a .. 2 .. b|]
+    else
+        [|a + 1 .. 2 .. b|]
